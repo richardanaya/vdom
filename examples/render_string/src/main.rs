64b5e9f70d5836ed3b220ae3ae062e14304e6045
Richard <Richard>
2020-07-28 12:36:55 -0700

    todo

--- examples/render_string/src/main.rs
+++ examples/render_string/src/main.rs
@@ -1,10 +1,44 @@
 use vdom::*;
 
+
+pub struct TodoItem;
+
+impl TodoItem {
+  fn new(s:&str) -> VNode{
+    view! {
+      Div.add_class("todo-item"){
+        Text(s)
+      }
+    }
+  }
+}
+
+pub struct TodoList;
+
+impl TodoList {
+  fn new(todos:Vec<String>) -> VNode{
+    view! {
+      Div.add_class("todo-list"){
+        For(i in todos.iter()) { TodoItem(i) }
+      }
+    }
+  }
+}
+
 fn main() {
-    let html = view! {
-      Div.add_class("greeting"){
-        Text("Hello World!")
+    let todos = vec![
+      "say hello".to_string(),
+      "write some code".to_string(),
+      "eat lunch".to_string(),
+    ];
+
+    let list = TodoList::new(todos);
+
+    let my_app = view! {
+      Div.add_class("app"){
+        list
       }
     };
-    println!("{}", html.render_to_string());
+
+    println!("{}", my_app.render_to_string());
 }
